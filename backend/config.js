1644415071a9b4fb331ad5c216bde6a2a27d8046
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-10 20:10:58 +0545

    mongodb connection initialised

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 3003;
+
+const db_URL =
+  "mongodb+srv://siddhantpandey0922:UcqQdBnFgBlyXhsF@clusterbookstore.4tkx5.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBookStore";
