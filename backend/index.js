9fe2c6eea79b972c263f01fdf2bd6aae0d56a383
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-10 20:00:09 +0545

    get request test

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("getting the reques");
+  res.status(200).json({ message: "hello" });
+});
+
 app.listen(PORT, () => {
   console.log(`Successfully connected to the port : ${PORT}`);
 });
