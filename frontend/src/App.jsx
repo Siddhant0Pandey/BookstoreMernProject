fd1ecb3699c34a56b831942c8641d0878020d3fd
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-22 12:18:21 +0545

    router setup for spa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import ShowBook from "./pages/ShowBook";
 
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/create" element={<CreateBook />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+      </Routes>
     </>
   );
 }
