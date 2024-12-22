226933d33093d53966bea13ecbb50ec316119b8f
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-22 21:21:03 +0545

    dialog box setup and style update

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,32 +3,40 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import MessageBox from "../components/message/MessageBox";
 
 const CreateBook = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [publishYear, setPublishYear] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const navigate = useNavigate();
 
   function handleSaveBook() {
-    const data = { title, author, publishYear };
-    setLoading(true);
     if (!title || !author || !publishYear) {
       alert("Please fill out all fields!");
       return;
     }
+
+    const data = { title, author, publishYear };
+    setLoading(true);
+
     axios
       .post(`http://localhost:8000/books`, data)
       .then(() => {
         setLoading(false);
+        setShowMessage(true);
         navigate("/");
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err);
         setLoading(false);
-        alert("Some error occured,Please try again!");
+        setErrorMessage(
+          "An error occurred while creating the book. Please try again."
+        );
       });
   }
 
@@ -36,48 +44,45 @@ const CreateBook = () => {
     <div className="p-4">
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
-      {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-blue-300  rounded-xl w-[600px] p-4 mx-auto">
+      {loading && <Spinner />}
+      <div className="flex flex-col border-2 border-blue-300 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500"> Title</label>
+          <label className="text-xl mr-4 text-gray-500">Title</label>
           <input
             type="text"
             value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
+            onChange={(e) => setTitle(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500"> Author</label>
+          <label className="text-xl mr-4 text-gray-500">Author</label>
           <input
             type="text"
             value={author}
-            onChange={(e) => {
-              setAuthor(e.target.value);
-            }}
+            onChange={(e) => setAuthor(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500"> Publish Year</label>
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
           <input
             type="text"
             value={publishYear}
-            onChange={(e) => {
-              setPublishYear(e.target.value);
-            }}
+            onChange={(e) => setPublishYear(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <button
           className="p-2 bg-sky-400 hover:opacity-80"
           onClick={handleSaveBook}
+          disabled={loading} // Disable button while loading
         >
           Save
         </button>
       </div>
+      {showMessage && <MessageBox message="Book created successfully!" />}
+      {errorMessage && <MessageBox message={errorMessage} />}
     </div>
   );
 };
