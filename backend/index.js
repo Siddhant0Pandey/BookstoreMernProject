f8b4cd2405ecdf60e0bbbbe69e07e815e93858dc
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-12-23 21:16:33 +0545

    refactored index ifle

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,21 @@ import express from "express";
 import { PORT, db_URL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/books.routes.js";
+import personRoute from "./routes/person.routes.js";
 import cors from "cors";
 import "dotenv/config";
+import { localAuthMiddleware } from "./auth/user.auth.js";
 
 const app = express();
 //
 app.use(cors());
 //
 
-app.use();
-
 app.use(express.json());
 app.use("/books", booksRoute);
+app.use("/person", personRoute);
 
-app.get("/", (req, res) => {
+app.get("/", localAuthMiddleware, (req, res) => {
   console.log("getting the reques");
   res.status(200).json({ message: "hello" });
 });
